91724047b72bda2b94a4221b272d5c4b71a83b7a
hyeong1 <mi75625@gmail.com>
2022-12-26 12:40:56 +0900

    오타 수정

--- src/chap_03/_Quiz_03.java
+++ src/chap_03/_Quiz_03.java
@@ -1,7 +1,7 @@
 package chap_03;
 
 public class _Quiz_03 {
-    // 주민등록번호에서 생년월일 및 성별까지말 출력하는 프로그램 작선
+    // 주민등록번호에서 생년월일 및 성별까지말 출력하는 프로그램 작성
     // 예: 901231-1234567 -> 901231-1까지 출력
     public static void main(String[] args) {
         String residentN1 = "901231-1234567";
