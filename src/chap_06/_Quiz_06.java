7ffae25831134aedef94c2280d98446be28a7007
hyeong1 <mi75625@gmail.com>
2022-12-26 12:56:23 +0900

    개인정보 비공개 처리하기 문제

--- src/chap_06/_Quiz_06.java
+++ src/chap_06/_Quiz_06.java
@@ -7,13 +7,19 @@ public class _Quiz_06 {
     // 주민등록번호: 9번째 글자
     // 전화번호: 10번째 글자 부터 비공개
     public static String getHiddenData(String str, int n) {
-        return
+        String hiddenStr = str.substring(0, n); // 공개하는 문자만 보여주도록 문자열 잘라서 새로운 문자열 hiddenStr에 저장
+        for (int i = n;i < str.length();i++) // 문자열에 비공개 처리할 문자들은 "*"로 추가
+            hiddenStr += "*";
+
+        return hiddenStr; // 완성된 문자열 반환
     }
     public static void main(String[] args) {
         String name = "이민형";
         String id = "011114-4012345";
         String phone = "010-1234-5678";
 
-        System.out.println("이름 : " + );
+        System.out.println("이름 : " + getHiddenData(name, 1));
+        System.out.println("주민등록번호 :" + getHiddenData(id, 8));
+        System.out.println("전화번호 : " + getHiddenData(phone, 9));
     }
 }
